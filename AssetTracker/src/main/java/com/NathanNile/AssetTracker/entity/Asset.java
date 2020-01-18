53a1ff407bee9b1fc192b161210e1034eef2e4c1
nathan-pc <nn5g14@soton.ac.uk>
2020-01-18 17:59:25 +0000

    Added bookings list on Asset entity with join to asset id

--- AssetTracker/src/main/java/com/NathanNile/AssetTracker/entity/Asset.java
+++ AssetTracker/src/main/java/com/NathanNile/AssetTracker/entity/Asset.java
@@ -1,11 +1,18 @@
 package com.NathanNile.AssetTracker.entity;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
 
 @Entity
 @Table(name = "asset_table")
@@ -27,6 +34,10 @@ public class Asset {
 	
 	@Column(name = "home_location")
 	private String homeLocation;
+	
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "asset_id")
+	private List<Booking> bookings;
 	
 	public Asset () {
 	}
