339fff6ebfb544ea18addfeb7bb1b0dff7eaa1e0
nathan-pc <nn5g14@soton.ac.uk>
2020-01-19 16:32:20 +0000

    AssetServiceImpl created

---
+++ AssetTracker/src/main/java/com/NathanNile/AssetTracker/service/AssetServiceImpl.java
@@ -0,0 +1,36 @@
+package com.NathanNile.AssetTracker.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.NathanNile.AssetTracker.entity.Asset;
+
+@Service
+public class AssetServiceImpl implements AssetService {
+
+	@Override
+	public List<Asset> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Asset findById(int theId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(Asset theAsset) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(int theId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
