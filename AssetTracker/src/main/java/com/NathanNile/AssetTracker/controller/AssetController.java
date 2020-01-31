69c919cb5451a460a0e9f27d4dfd13e99dde57df
nathan-pc <nn5g14@soton.ac.uk>
2020-01-31 21:19:47 +0000

    removed unused import

--- AssetTracker/src/main/java/com/NathanNile/AssetTracker/controller/AssetController.java
+++ AssetTracker/src/main/java/com/NathanNile/AssetTracker/controller/AssetController.java
@@ -1,6 +1,5 @@
 package com.NathanNile.AssetTracker.controller;
 
-import java.net.http.HttpClient.Redirect;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
